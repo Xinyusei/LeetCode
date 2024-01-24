a220f7e1dbb3e70fb95f6c2358d57e9578133fcf
Yuse1 <1623500565@qq.com>
2024-01-24 19:40:53 +0800

    ez-考察string.replace()方法

---
+++ lcRating/lt1200/a1108IP地址无效化.java
@@ -0,0 +1,25 @@
+package lcRating.lt1200;
+
+/**
+ * 1108. IP 地址无效化
+ * https://leetcode.cn/problems/defanging-an-ip-address/
+ */
+public class a1108IP地址无效化 {
+    /*
+    public String defangIPaddr(String address) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : address.toCharArray()) {
+            if(c != '.'){
+                sb.append(c);
+            }
+            else{
+                sb.append("[.]");
+            }
+        }
+        return sb.toString();
+    }
+    */
+    public String defangIPaddr(String address) {
+        return address.replace(".", "[.]");
+    }
+}
