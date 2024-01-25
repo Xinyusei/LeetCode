33b3b85cc17b6d4168ded7fa281819f8ff66eb10
Yuse1 <1623500565@qq.com>
2024-01-25 14:44:47 +0800

    ez-简单模拟

---
+++ lcRating/lt1200/a1450在既定时间做作业的学生人数.java
@@ -0,0 +1,16 @@
+package lcRating.lt1200;
+
+/**
+ * 1450. 在既定时间做作业的学生人数
+ * https://leetcode.cn/problems/number-of-students-doing-homework-at-a-given-time/
+ */
+public class a1450在既定时间做作业的学生人数 {
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int n = startTime.length, res = 0;
+        for (int i = 0; i < n; i++) {
+            if (queryTime >= startTime[i] && queryTime <= endTime[i])
+                res++;
+        }
+        return res;
+    }
+}
