977ed948caea50c849f54d323dc66e586831244b
Yuse1 <1623500565@qq.com>
2024-01-25 15:53:17 +0800

    ez-好题-位运算,bitCount

--- Ev2024_1/a1_25ez.java
+++ Ev2024_1/a1_25ez.java
@@ -20,10 +20,25 @@ public class a1_25ez {
     private int getPos(int num) {
         int pos = 0;
         while (num > 0) {
-            if ((num & 1) != 0)
+            /*
+            当x = 2^n(n为自然数)时，
+            a % x = a & (x  - 1 )
+            */
+            if ((num & 1) != 0) //等价于 num % 2
                 pos++;
             num >>= 1;
         }
         return pos;
+
+    }
+
+    public int sumIndicesWithKSetBits2(List<Integer> nums, int k) {
+        int n = nums.size(), ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (Integer.bitCount(i) == k) {
+                ans += nums.get(i);
+            }
+        }
+        return ans;
     }
 }
