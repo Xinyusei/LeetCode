3ef6cf95c533454399fda0299839e61737a41c99
Yuse1 <1623500565@qq.com>
2024-01-26 18:59:38 +0800

    ez-好题,求位数,位运算

---
+++ lcRating/lt1200/a1295统计位数为偶数的数字.java
@@ -0,0 +1,20 @@
+package lcRating.lt1200;
+
+/**
+ * 1295. 统计位数为偶数的数字
+ * https://leetcode.cn/problems/find-numbers-with-even-number-of-digits/
+ */
+public class a1295统计位数为偶数的数字 {
+    public int findNumbers(int[] nums) {
+        int count = 0;
+        for (int num : nums) {
+            if ((getPNum(num) & 1) == 0)
+                count++;
+        }
+        return count;
+    }
+
+    private int getPNum(int num) {
+        return (int) Math.log10(num) + 1;
+    }
+}
