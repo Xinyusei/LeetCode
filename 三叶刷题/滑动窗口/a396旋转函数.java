90466c0fa74158299b730e103ac94593abaf3a52
Yuse1 <1623500565@qq.com>
2024-02-01 19:54:57 +0800

    mid-好题-滑动窗口+前缀和已完成,TODO 动态规划

---
+++ 三叶刷题/滑动窗口/a396旋转函数.java
@@ -0,0 +1,50 @@
+package 三叶刷题.滑动窗口;
+
+public class a396旋转函数 {
+    /**
+     * @param nums
+     * @return
+     */
+    public int maxRotateFunction(int[] nums) {
+        /**
+         * 前缀和+滑动窗口
+         */
+        int n = nums.length, cur = 0;
+        int[] extend = new int[2 * n + 1];
+        int[] pre = new int[2 * n + 1];
+        //扩展数组
+        for (int i = 0; i < extend.length; i++) {
+            extend[i] = nums[i % n];
+        }
+        //构造前缀和
+        for (int i = 1; i <= 2 * n; i++) {
+            pre[i] = pre[i - 1] + extend[i - 1];
+        }
+        //起始cur
+        for (int i = 0; i < n; i++) {
+            cur += nums[i] * i;
+        }
+        int ans = cur;
+        for (int i = 1, j = i + n - 1; i < n; i++, j++) {
+            cur -= pre[j] - pre[i];
+            cur += extend[j] * (n - 1);
+            if (cur > ans) ans = cur;
+        }
+        return ans;
+    }
+    /**
+     * O(n²)-超时
+     */
+
+//    public int maxRotateFunction(int[] nums) {
+//        int ans = Integer.MIN_VALUE;
+//        for (int i = 0; i < nums.length; i++) {
+//            int f = 0;
+//            for (int j = 0; j < nums.length; j++) {
+//                f += nums[j] * ((j + i) % nums.length);
+//            }
+//            ans = Math.max(ans, f);
+//        }
+//        return ans;
+//    }
+}
