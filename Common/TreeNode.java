06b3ed8bcf91714ae61b582941706d9d02bacb01
Yuse1 <1623500565@qq.com>
2024-02-23 15:11:02 +0800

    将构造方法改为public

--- Common/TreeNode.java
+++ Common/TreeNode.java
@@ -4,11 +4,11 @@ public class TreeNode {
     public int val;
     public TreeNode left;
     public TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) {
+    public TreeNode() {}
+    public TreeNode(int val) {
         this.val = val;
     }
-    TreeNode(int val, TreeNode left, TreeNode right) {
+    public TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
         this.right = right;
