80b8889a7b026dfdb7cfdca7c30bff6d84b89401
Yuse1 <1623500565@qq.com>
2024-03-01 20:46:51 +0800

    mid - 二叉树公共祖先(两个节点)-改进

--- Type_/二叉树/a236二叉树的最近公共祖先.java
+++ Type_/二叉树/a236二叉树的最近公共祖先.java
@@ -12,17 +12,27 @@ import java.util.List;
 public class a236二叉树的最近公共祖先 {
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null || root == p || root == q)
-            return root;
-
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
-
-        if (left != null && right != null) {
-            return root;
-        }
-        if (left == null && right == null)
-            return null;
-        return left == null ? right : left;
+        return find(root, p.val, q.val);
     }
+
+    public TreeNode find(TreeNode node, int val1, int val2) {
+        if (node == null)
+            return null;
+
+        //如果当前node就是p或者q
+        if (node.val == val1 || node.val == val2)
+            return node;
+        //在node的左子树是否存在p或q
+        TreeNode left = find(node.left, val1, val2);
+        //在node的左子树是否存在p或q
+        TreeNode right = find(node.right, val1, val2);
+
+        //p、q分居在node的两侧
+        if (left != null && right != null)
+            return node;
+        //p 、 q位于node.left 或者node.right子树
+        return left == null ? right : left;
+
+    }
+
 }
