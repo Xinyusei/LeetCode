d833c31206777af50896344754e9a5c2a6262b93
Yuse1 <1623500565@qq.com>
2024-03-09 20:30:21 +0800

    mid - 二叉树最大宽度 - BFS/DFS+编码 很重要

--- Type_/二叉树/a662二叉树最大宽度.java
+++ Type_/二叉树/a662二叉树最大宽度.java
@@ -2,6 +2,7 @@ package Type_.二叉树;
 
 import Common.TreeNode;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
@@ -11,6 +12,12 @@ import java.util.Objects;
  * https://leetcode.cn/problems/maximum-width-of-binary-tree/description/
  */
 public class a662二叉树最大宽度 {
+    /**
+     * 层序遍历 + 编号
+     *
+     * @param root
+     * @return
+     */
     public int widthOfBinaryTree(TreeNode root) {
         //  1
         // 2 3
@@ -35,5 +42,46 @@ public class a662二叉树最大宽度 {
             }
         }
         return res;
+    }
+
+
+    //记录 每层出现最小的编号
+    private final HashMap<Integer, Integer> map = new HashMap<>();
+    //层数
+    private int deep = 0;
+
+    //结果
+    private int result = 0;
+
+    /**
+     * DFS + 编号
+     * @param root
+     * @return
+     */
+    public int widthOfBinaryTree2(TreeNode root) {
+        traverse(root);
+        return result;
+    }
+
+    //前序遍历 根左右
+    public void traverse(TreeNode root) {
+        if (root == null)
+            return;
+
+        deep++;
+        if (root.left != null)
+            root.left.val = root.val * 2;
+        if (root.right != null)
+            root.right.val = root.val * 2 + 1;
+        //刚进入的时候一定是该层最小的编号
+        map.putIfAbsent(deep, root.val);
+        //这一层每遇到一个节点就计算其与最小编号的差
+        result = Math.max(result, root.val - map.get(deep) + 1);
+
+        traverse(root.left);
+        traverse(root.right);
+
+        deep--;
+
     }
 }
