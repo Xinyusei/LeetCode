7e4ec9fb017719a522c830b28b2fe517ebe97e48
Yuse1 <1623500565@qq.com>
2024-04-03 21:49:28 +0800

    修改了逻辑 - 很经典，必做！

--- Type_/二叉树/a669修剪二叉搜索树.java
+++ Type_/二叉树/a669修剪二叉搜索树.java
@@ -9,51 +9,40 @@ import Common.TreeNode;
 public class a669修剪二叉搜索树 {
 
     //方法2-前序遍历
+    //返回以当前节点为根的树处理完毕之后的根节点
     public TreeNode trimBST(TreeNode root, int low, int high) {
-        return traverse(root, low, high);
-    }
-
-    /**
-     *
-     * @param node
-     * @param low
-     * @param high
-     * @return 返回以当前节点为根的树处理完毕之后的根节点
-     */
-    private TreeNode traverse(TreeNode node, int low, int high) {
-        if (node == null)
+        if (root == null)
             return null;
 
-        if (node.val < low)
-            //直接返回root.right 等于删除root以及root的左子树
-            return traverse(node.right, low, high);
-        if (node.val > high)
-            //直接返回root.left 等于删除root以及root的右子树
-            return traverse(node.left, low, high);
-        // 闭区间内的节点无需处理
-        node.left = traverse(node.left, low, high);
-        node.right = traverse(node.right, low, high);
-        return node;
+        if (root.val < low) {
+            return trimBST(root.right, low, high);
+        } else if (root.val > high) {
+            return trimBST(root.left, low, high);
+        }
+
+        root.left = trimBST(root.left, low, high);
+        root.right = trimBST(root.right, low, high);
+        return root;
     }
+
 
     //方法1-后序遍历
 
-//    public TreeNode trimBST(TreeNode root, int low, int high) {
-//        return traverse(root,low,high);
-//    }
-//    private TreeNode traverse(TreeNode node, int low, int high) {
-//        if (node == null)
-//            return null;
-//
-//        node.left = traverse(node.left, low, high);
-//        node.right = traverse(node.right, low, high);
-//        //后序位置-跟子树有关,需要给函数设置返回值
-//        if (node.val < low)
-//            node = node.right;
-//        else if (node.val > high)
-//            node = node.left;
-//        return node;
-//    }
+    class Solution2 {
+        public TreeNode trimBST(TreeNode root, int low, int high) {
+            if (root == null)
+                return null;
+
+            root.left = trimBST(root.left, low, high);
+            root.right = trimBST(root.right, low, high);
+
+            if (root.val < low)
+                root = root.right;
+            else if (root.val > high)
+                root = root.left;
+            return root;
+        }
+    }
 
 
 }
