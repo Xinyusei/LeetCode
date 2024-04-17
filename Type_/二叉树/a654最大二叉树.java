45fee53ea5ef9c5c7a9cd6c48028e59b49e2fb6f
Yuse1 <1623500565@qq.com>
2024-04-17 15:11:25 +0800

    二叉树 - MID - 二叉树构造,二刷。

--- Type_/二叉树/a654最大二叉树.java
+++ Type_/二叉树/a654最大二叉树.java
@@ -10,41 +10,29 @@ import java.util.Arrays;
  */
 public class a654最大二叉树 {
     public TreeNode constructMaximumBinaryTree(int[] nums) {
-        //base case
-//        if (nums.length == 0)
-//            return null;
-//
-//        int idx = 0;
-//        for (int i = 1; i < nums.length; i++) {
-//            if (nums[idx] > nums[i])
-//                idx = i;
-//        }
-//        TreeNode node = new TreeNode(nums[idx]);
-//        node.left = constructMaximumBinaryTree(Arrays.copyOfRange(nums, 0, idx));
-//        node.right = constructMaximumBinaryTree(Arrays.copyOfRange(nums, idx + 1, nums.length));
-//        return node;
-
         return construct(nums, 0, nums.length - 1);
     }
-
-
+    /**
+     * 构造二叉树
+     *
+     * @param nums
+     * @param lo
+     * @param hi
+     * @return
+     */
     TreeNode construct(int[] nums, int lo, int hi) {
         if (lo > hi)
             return null;
-
-        int idx = -1, maxVal = Integer.MIN_VALUE;
+        int idx = -1, MaxVal = Integer.MIN_VALUE;
         for (int i = lo; i <= hi; i++) {
-            if (nums[i] > maxVal) {
-                maxVal = nums[i];
+            if (nums[i] > MaxVal) {
+                MaxVal = nums[i];
                 idx = i;
             }
         }
-        TreeNode root = new TreeNode(maxVal);
+        TreeNode root = new TreeNode(nums[idx]);
         root.left = construct(nums, lo, idx - 1);
         root.right = construct(nums, idx + 1, hi);
-
         return root;
-
-
     }
 }
