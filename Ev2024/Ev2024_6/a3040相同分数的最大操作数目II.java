b4b148a9673eba0cd4e45c42a8911dde8765a8ad
Yuse1 <1623500565@qq.com>
2024-06-10 17:19:47 +0800

    MID - 区间DP - 待做

---
+++ Ev2024/Ev2024_6/a3040相同分数的最大操作数目II.java
@@ -0,0 +1,12 @@
+package Ev2024.Ev2024_6;
+
+/**
+ * https://leetcode.cn/problems/maximum-number-of-operations-with-the-same-score-ii/?envType=daily-question&envId=2024-06-10
+ */
+public class a3040相同分数的最大操作数目II {
+    class Solution {
+        public int maxOperations(int[] nums) {
+            
+        }
+    }
+}
