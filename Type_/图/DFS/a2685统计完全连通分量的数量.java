be505102728969d461a24d6c0fe74bc7f2af9ae0
Yuse1 <1623500565@qq.com>
2024-07-08 17:42:22 +0800

    图论 - MID - 判断是否是完全连通分量 - 重要

---
+++ Type_/图/DFS/a2685统计完全连通分量的数量.java
@@ -0,0 +1,48 @@
+package Type_.图.DFS;
+
+import java.util.*;
+
+/**
+ * <a href="https://leetcode.cn/problems/count-the-number-of-complete-components/description/">2685. 统计完全连通分量的数量</a>
+ */
+public class a2685统计完全连通分量的数量 {
+    class Solution {
+
+        private int curV,curE;
+
+        public int countCompleteComponents(int n, int[][] edges) {
+            List<Integer>[] g = new List[n];
+            Arrays.setAll(g, e -> new ArrayList<>());
+            //建图
+            for (int[] e : edges) {
+                int x = e[0], y = e[1];
+                g[x].add(y);
+                g[y].add(x);
+            }
+            boolean[] vis = new boolean[n];
+            int res = 0;
+            for (int i = 0; i < n; i++) {
+                if (!vis[i]) {
+                    curV = 0;
+                    curE = 0;
+                    traverse(g, vis, i);
+                    if (curV * (curV - 1) == curE)
+                        res++;
+                }
+            }
+            return res;
+        }
+
+        private void traverse(List<Integer>[] g, boolean[] vis, int cur) {
+            if(vis[cur])
+                return;
+            vis[cur] = true;
+            curV++;
+            List<Integer> edges = g[cur];
+            curE += edges.size();
+            for (int y : edges) {
+                traverse(g, vis, y);
+            }
+        }
+    }
+}
