3bbc263299cc0b9d77f5a0f1b51d40d27ef2ed5c
Yuse1 <1623500565@qq.com>
2024-07-15 15:32:57 +0800

    EZ - 前缀和的简单应用

---
+++ Type_/动态规划/最大子数组_子段和/a1191K次串联后最大子数组之和.java
@@ -0,0 +1,15 @@
+package Type_.动态规划.最大子数组_子段和;
+
+/**
+ * https://leetcode.cn/problems/k-concatenation-maximum-sum/description/
+ */
+public class a1191K次串联后最大子数组之和 {
+    class Solution {
+        static final int MOD = (int) 1e9 + 7;
+
+        public int kConcatenationMaxSum(int[] s, int k) {
+
+            return 0;
+        }
+    }
+}
