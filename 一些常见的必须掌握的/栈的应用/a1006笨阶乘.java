f2cb6cba104fa48f495e5a9d59f118f9f4f24a66
Yuse1 <1623500565@qq.com>
2024-07-15 11:53:40 +0800

    MID - 用栈模拟运算顺序,必须掌握

---
+++ 一些常见的必须掌握的/栈的应用/a1006笨阶乘.java
@@ -0,0 +1,37 @@
+package 一些常见的必须掌握的.栈的应用;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 1006. 笨阶乘
+ * https://leetcode.cn/problems/clumsy-factorial/description/
+ */
+public class a1006笨阶乘 {
+    class Solution {
+        public int clumsy(int n) {
+            List<Integer> stack = new LinkedList<>();
+            stack.addFirst(n);
+            n--;
+
+            int idx = 0;
+            while (n >= 1) {
+                if (idx % 4 == 0) //乘
+                    stack.addFirst(stack.removeFirst() * n);
+                else if (idx % 4 == 1) //除
+                    stack.addFirst(stack.removeFirst() / n);
+                else if (idx % 4 == 2) //加
+                    stack.addFirst(n);
+                else
+                    stack.addFirst(-n);
+                idx++;
+                n--;
+            }
+            int res = stack.removeFirst();
+            while (!stack.isEmpty()) {
+                res += stack.removeFirst();
+            }
+            return res;
+        }
+    }
+}
